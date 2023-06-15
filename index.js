5bc2ddc947b730b09c795aa6472c4c1fd6d76cf5
Sush Singh <sushantsingh4720@gmail.com>
2023-06-15 09:42:26 +0530

    api rate limit added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 import { config } from "dotenv";
 import cors from "cors";
 import dbConnect from "./config/dbConnect.js";
@@ -13,6 +14,14 @@ dbConnect();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const apiLimiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 10000,
+  message: "Too many request from this IP, please try again after an hour.",
+});
+// Apply the rate limiting middleware to API calls only
+app.use("/api", apiLimiter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "API is working" });
